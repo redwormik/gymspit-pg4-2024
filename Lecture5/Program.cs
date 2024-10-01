68971e6c08a7d4c3d07e450976217f5f238a53d7
Jakub Červenka <redwormik@gmail.com>
2024-10-01 09:25:54 +0200

    Add Lecture5

---
+++ Lecture5/Program.cs
@@ -0,0 +1,46 @@
+﻿static string ReadLine(string prompt)
+{
+	Console.Write("{0}: ", prompt);
+	return Console.ReadLine();
+}
+
+
+static string ReadWord()
+{
+	return ReadLine("Enter a word to translate");
+}
+
+
+static string ReadTranslation(string word)
+{
+	return ReadLine(string.Format("Enter a translation of \"{0}\"", word));
+}
+
+
+static string ReadQuery()
+{
+	return ReadLine("Enter a word to find");
+}
+
+IDictionary<string, string> dictionary = new Dictionary<string, string>();
+
+string word = ReadWord();
+while (word != "") {
+	string translation = ReadTranslation(word);
+	dictionary[word] = translation;
+	word = ReadWord();
+}
+
+string query = ReadQuery();
+while (query != "") {
+	if (dictionary.ContainsKey(query)) {
+		Console.WriteLine("Translation of \"{0}\" is \"{1}\".", query, dictionary[query]);
+	} else {
+		Console.WriteLine("Translation of \"{0}\" not found.", query);
+		string translation = ReadTranslation(query);
+		if (translation != "") {
+			dictionary[query] = translation;
+		}
+	}
+	query = ReadQuery();
+}
